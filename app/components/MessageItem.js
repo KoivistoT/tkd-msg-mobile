b519eb9afd8022ef816b2359827d7457a8c2502b
KoivistoT <70210410+KoivistoT@users.noreply.github.com>
2022-02-23 17:13:58 +0200

    Update MessageItem.js

--- app/components/MessageItem.js
+++ app/components/MessageItem.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity, Image } from "react-native";
 import colors from "../../config/colors";
 
 import AppText from "./AppText";
 function MessageItem({ item, userId }) {
+  console.log("tee avattava kuva");
   return (
     <TouchableOpacity
       style={item.postedByUser === userId ? styles.me : styles.otherUser}
     >
       <AppText key={item._id}>{item.messageBody}</AppText>
+      {item.imageURLs &&
+        item.imageURLs.length !== 0 &&
+        item.imageURLs.map((url) => (
+          <Image
+            style={{ width: 20, height: 20 }}
+            source={{ uri: url.toString() }}
+          />
+        ))}
     </TouchableOpacity>
   );
 }
