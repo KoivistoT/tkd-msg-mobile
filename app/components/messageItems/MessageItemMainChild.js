56fd58f97fd795d2d5c33a03894caf7be72cda70
KoivistoT <70210410+KoivistoT@users.noreply.github.com>
2022-03-31 11:22:32 +0300

    cleaner code

--- app/components/messageItems/MessageItemMainChild.js
+++ app/components/messageItems/MessageItemMainChild.js
@@ -21,6 +21,7 @@ import {
 import MessageItemReply from "./MessageItemReply";
 import { navigationRef } from "../../navigation/rootNavigation";
 import ShowDocumentModal from "../modals/ShowDocumentModal";
+import messageFuncs from "../../../utility/messageFuncs";
 
 function MessageItemMainChild({
   message,
@@ -57,71 +58,6 @@ function MessageItemMainChild({
     dispatch(deleteMessageById(roomId, messageId));
   };
   // console.log("message Child päivittyy---------------------");
-
-  // tämä kans muualla
-  const renderAutolinkText = (message, style) => {
-    var searchWordRexExp = "";
-    if (searchWord) {
-      searchWordRexExp = new RegExp(searchWord + "(.*?)", "i");
-    }
-    return (
-      <Autolink
-        style={style}
-        // Required: the text to parse for links
-        // text="This is the string to parse for urls (https://github.com/joshswan/react-native-autolink), phone numbers (415-555-5555), emails (josh@example.com), mentions/handles (@twitter), and hashtags (#exciting)"
-        // Optional: enable email linking
-        text={message}
-        // email
-        // Optional: enable hashtag linking to instagram
-        // hashtag="instagram"
-        // Optional: enable @username linking to twitter
-        // mention="twitter"
-        // Optional: enable phone linking
-        phone="true"
-        // Optional: enable URL linking
-        // url
-        // Optional: custom linking matchers
-        matchers={[
-          {
-            // //this is for italic
-            pattern: /\_(.*?)\_/,
-            style: { fontStyle: "italic" },
-            getLinkText: (replacerArgs) => `${replacerArgs[1]}`,
-          },
-          {
-            // //this is for bolding
-            pattern: /\*(.*?)\*/,
-            style: { fontWeight: "bold" },
-            getLinkText: (replacerArgs) => `${replacerArgs[1]}`,
-          },
-          {
-            //   // //this is for searchword
-
-            pattern: new RegExp(searchWordRexExp),
-            style: { color: "red", fontWeight: "bold" },
-            // getLinkText: (replacerArgs) => `${replacerArgs[1]}`,
-          },
-          {
-            pattern:
-              /(\+\d{1,3}\s?)?((\(\d{3}\)\s?)|(\d{3})(\s|-?))(\d{3}(\s|-?))(\d{4})(\s?(([E|e]xt[:|.|]?)|x|X)(\s?\d+))?/g,
-            style: { color: "blue" },
-            // getLinkText: (replacerArgs) => `@${replacerArgs[1]}`,
-            onPress: (match) => {
-              const phoneNumber = match.replacerArgs[0];
-
-              Linking.canOpenURL(`tel:${phoneNumber}`).then((supported) => {
-                if (!supported) {
-                  // handle the error
-                } else {
-                  return Linking.openURL(`tel:${phoneNumber}`);
-                }
-              });
-            },
-          },
-        ]}
-      />
-    );
-  };
 
   return (
     <>
@@ -163,7 +99,9 @@ function MessageItemMainChild({
             onScrollToIndex={onScrollToIndex}
           />
         )}
-        <AppText>{renderAutolinkText(messageBody)}</AppText>
+        <AppText>
+          {messageFuncs.autolinkText(messageBody, null, searchWord)}
+        </AppText>
       </TouchableOpacity>
     </>
   );
