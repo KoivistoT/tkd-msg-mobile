02180344b2e7939dc3fa3a3f59e4fda47ffd7ba1
KoivistoT <70210410+KoivistoT@users.noreply.github.com>
2022-04-12 12:46:15 +0300

    cleaner code

--- app/components/messageItems/MessageItemImage.js
+++ app/components/messageItems/MessageItemImage.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef } from "react";
-import { View, StyleSheet, Text, TouchableOpacity, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, TouchableOpacity, Image } from "react-native";
 import colors from "../../../config/colors";
 
 import AppText from "../AppText";
