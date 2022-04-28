9632c07447cce1caeac2a2a3765f924a06ddf014
KoivistoT <70210410+KoivistoT@users.noreply.github.com>
2022-04-28 13:43:54 +0300

    Update CreateChannelScreen.js

--- screens/CreateChannelScreen.js
+++ screens/CreateChannelScreen.js
@@ -1,30 +1,9 @@
-import React, { useState } from "react";
-
-import Platform from "react-native";
-import {
-  StyleSheet,
-  Modal,
-  TouchableOpacity,
-  View,
-  ScrollView,
-} from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-
-import { Entypo } from "@expo/vector-icons";
-
-import colors from "../config/colors";
+import React from "react";
 import CreateChannelForm from "../app/components/forms/CreateChannelForm";
-import AppButton from "../app/components/AppButton";
-import Screen from "../app/components/Screen";
 
 function CreateChannelScreen({}) {
   return <CreateChannelForm />;
 }
-
-const styles = StyleSheet.create({
-  modal: {},
-  button: {},
-});
 
 function areEqual(prevProps, nextProps) {
   return true;
