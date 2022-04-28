eea52a11b76d4131c685f9cde2341600db755005
KoivistoT <70210410+KoivistoT@users.noreply.github.com>
2022-04-28 14:51:39 +0300

    cleaner code

--- api/messages.js
+++ api/messages.js
@@ -1,38 +1,38 @@
-import client from "./client";
+// import client from "./client";
 
-const endpoint = "/messages/";
-const getMessages = () => client.get(endpoint);
-const addMessageListener = () => client.get(endpoint + "/addMsgListener");
+// const endpoint = "/messages/";
+// const getMessages = () => client.get(endpoint);
+// const addMessageListener = () => client.get(endpoint + "/addMsgListener");
 
-const addMessage = (message, token) => {
-  //   const data = new FormData();
-  //   //muuta messagen message text, ettei tule message.message
-  //   data.append("message", message);
-  //   data.append("date", Date.now());
-  //     console.log(data);
-  const data = {
-    message,
-    date: Date.now(),
-  };
+// const addMessage = (message, token) => {
+//   //   const data = new FormData();
+//   //   //muuta messagen message text, ettei tule message.message
+//   //   data.append("message", message);
+//   //   data.append("date", Date.now());
+//   //     console.log(data);
+//   const data = {
+//     message,
+//     date: Date.now(),
+//   };
 
-  return client.post(endpoint, data);
-};
+//   return client.post(endpoint, data);
+// };
 
-const sendMessage = (
-  message = "jaaha",
-  roomId = "61d35b8945d1e3e2bc83ff10"
-) => {
-  //   const data = new FormData();
-  //   //muuta messagen message text, ettei tule message.message
-  //   data.append("message", message);
-  //   data.append("date", Date.now());
-  //     console.log(data);
-  const data = {
-    messageBody: message,
-    date: Date.now(),
-    roomId,
-  };
-  return client.post(endpoint, data);
-};
+// const sendMessage = (
+//   message = "jaaha",
+//   roomId = "61d35b8945d1e3e2bc83ff10"
+// ) => {
+//   //   const data = new FormData();
+//   //   //muuta messagen message text, ettei tule message.message
+//   //   data.append("message", message);
+//   //   data.append("date", Date.now());
+//   //     console.log(data);
+//   const data = {
+//     messageBody: message,
+//     date: Date.now(),
+//     roomId,
+//   };
+//   return client.post(endpoint, data);
+// };
 
-export default { sendMessage, getMessages, addMessage, addMessageListener };
+// export default { sendMessage, getMessages, addMessage, addMessageListener };
