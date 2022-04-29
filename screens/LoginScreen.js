381bb4c7a10bdabb94500e65647fcafea0a8838e
KoivistoT <70210410+KoivistoT@users.noreply.github.com>
2022-04-29 10:08:28 +0300

    loginscreen final

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-
-import { View, StyleSheet, Image, Dimensions } from "react-native";
+import { View, StyleSheet } from "react-native";
 import * as Yup from "yup";
 import Screen from "../app/components/Screen";
 import ErrorMessage from "../app/components/ErrorMessage";
@@ -10,55 +9,38 @@ import SubmitButton from "../app/components/forms/SubmitButton";
 import AppKeyboardDismiss from "../app/components/AppKeyboardDismiss";
 import AppLoadIndicator from "../app/components/AppLoadIndicator";
 import { useDispatch, useSelector, useStore } from "react-redux";
-import {
-  clearErrorMessage,
-  errorMessageCleared,
-  login,
-} from "../store/currentUser";
-import { createSocketConnection } from "../store/socket";
+import { login } from "../store/currentUser";
 import asyncStorageFuncs from "../utility/asyncStorageFuncs";
-import AppText from "../app/components/AppText";
-import colors from "../config/colors";
+import AppSplashScreen from "./AppSplashScreen";
 
 const validationSchema = Yup.object().shape({
   userName: Yup.string().required().email().label("Username"),
   password: Yup.string().required().min(5).label("Password"),
 });
 
-function LoginScreen({ navigation }) {
-  const store = useStore();
-  const [loading, setLoading] = useState(false);
-  // const store = useStore();
-  // console.log(store.getState());
-
-  const isLoginFailed = useSelector((state) => state.auth.currentUser.error);
-
+function LoginScreen({}) {
   const dispatch = useDispatch();
 
-  const handleSubmit = async ({ userName, password }) => {
-    // dispatch(errorMessageCleared()); // tämän voisti tehdä onStart:issa
-    // asyncStorageFuncs.setData("autoLogin", true);
-    asyncStorageFuncs.setData("loginData", { userName, password });
+  const [isAutoLoginChecked, setIsAutoLoginChecked] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const isLoginFailed = useSelector((state) => state.auth.currentUser.error);
 
+  const handleSubmit = async ({ userName, password }) => {
+    asyncStorageFuncs.setData("loginData", { userName, password });
     setLoading(true);
     dispatch(login(userName, password));
   };
 
-  const [isAutoLoginChecked, setIsAutoLoginChecked] = useState(false);
-
   useEffect(() => {
     checkAutologin();
-
     if (isLoginFailed) {
       setIsAutoLoginChecked(true);
     }
   }, [isLoginFailed]);
 
-  // tämä voi olla muualla
   const checkAutologin = async () => {
     try {
       const autoLogin = await asyncStorageFuncs.getData("autoLogin");
-
       const loginData = await asyncStorageFuncs.getData("loginData");
 
       if (autoLogin && loginData) {
@@ -72,29 +54,12 @@ function LoginScreen({ navigation }) {
 
   return (
     <Screen style={styles.container}>
-      {!isAutoLoginChecked && !isLoginFailed && (
-        <View
-          style={{
-            position: "absolute",
-            backgroundColor: colors.white,
-            zIndex: 2,
-            width: "100%",
-            height: "100%",
-            justifyContent: "center",
-          }}
-        >
-          <Image
-            source={require("../assets/splash.png")}
-            defaultSource={require("../assets/splash.png")}
-          />
-        </View>
-      )}
+      {!isAutoLoginChecked && !isLoginFailed && <AppSplashScreen />}
       {isAutoLoginChecked && (
         <AppKeyboardDismiss>
-          <View style={{ marginTop: 100, alignSelf: "center", padding: "10%" }}>
+          <View style={styles.formContainer}>
             <AppForm
               initialValues={{ userName: "timon@posti.fi", password: "12345" }}
-              // initialValues={{ userName: "joo@joo.fi", password: "111111" }}
               onSubmit={handleSubmit}
               validationSchema={validationSchema}
             >
@@ -121,13 +86,8 @@ function LoginScreen({ navigation }) {
                   textContentType="password"
                 />
                 <ErrorMessage error={isLoginFailed} visible={isLoginFailed} />
-                {/* <TouchableOpacity
-            style={{ alignSelf: "flex-start", marginRight: 10 }}
-            onPress={() => navigation.navigate("Reset_password")}
-          >
-            <AppText>Reset password</AppText>
-          </TouchableOpacity> */}
-                <View style={{ flexDirection: "row", alignSelf: "flex-end" }}>
+
+                <View style={styles.buttonContainer}>
                   {loading && !isLoginFailed && <AppLoadIndicator />}
                   <SubmitButton title="LOGIN" />
                 </View>
@@ -142,10 +102,9 @@ function LoginScreen({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
-    // padding: 20,
-    // paddingTop: 200,
-    // marginHorizontal: 10,
     flex: 1,
   },
+  formContainer: { marginTop: 100, alignSelf: "center", padding: "10%" },
+  buttonContainer: { flexDirection: "row", alignSelf: "flex-end" },
 });
 export default LoginScreen;
