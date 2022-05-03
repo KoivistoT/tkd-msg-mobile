c019290739b92dac61e02e203304f57e4c44b3d3
KoivistoT <70210410+KoivistoT@users.noreply.github.com>
2022-05-03 10:35:53 +0300

    cleaner code

--- store/configureStore.js
+++ store/configureStore.js
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import toast from "./middleware/toast";
 import api from "./middleware/api";
-// import init from "./middleware/init";
 
 export default function () {
   return configureStore({
